de351357908fc7ed4e3e945650b8314455654a33
Jan Schenkel <jan.schenkel@quartam.com>
2013-07-09 04:59:16 +0200

    Added missing GPL3 headers to new source file

--- java/qrtjvmcore/src/com/quartam/external/ExternalAlias.java
+++ java/qrtjvmcore/src/com/quartam/external/ExternalAlias.java
@@ -1,3 +1,20 @@
+/* Copyright (C) 2008-2013 Quartam Software / Jan Schenkel.
+ 
+ This file is part of the [qrtjvm] project, also known as
+ Quartam JavaVirtualMachine External for LiveCode.
+ 
+ [qrtjvm] is free software; you can redistribute it and/or modify it under
+ the terms of the GNU General Public License v3 as published by the Free
+ Software Foundation.
+ 
+ [qrtjvm] is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with [qrtjvm].  If not see <http://www.gnu.org/licenses/>.  */
+
 package com.quartam.external;
 
 import java.lang.annotation.ElementType;
